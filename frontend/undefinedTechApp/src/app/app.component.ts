f33aea0d9fe50d59656fad6a3711464d7d191a56
KevinEpn <kevin.asimbaya@epn.edu.ec>
2025-01-25 16:26:35 -0500

    Código de interfaz actualizado con varios cambios, incluyendo actualizaciones de la interfaz de usuario, gestión del tamaño de fuente y cambio de idioma de la app

--- frontend/undefinedTechApp/src/app/app.component.ts
+++ frontend/undefinedTechApp/src/app/app.component.ts
@@ -1,20 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, CommonModule],
+  imports: [RouterOutlet, RouterLink, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'undefinedTechApp';
 
+  fontSize: number = 16; //tamaño inicial de fuente
+  fontSizeMin = 10;
+  fontSizeMax = 30;
+
   menuOption: string = '';
 
   onOption(menuOption: string) {
     this.menuOption = menuOption;
+  }
+
+  applyGlobalFontSize() {
+    document.documentElement.style.setProperty('--global-font-size', `${this.fontSize}px`);
   }
 }
--- frontend/undefinedTechApp/src/app/pages/home/home.component.ts
+++ frontend/undefinedTechApp/src/app/pages/home/home.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  menuOption: string = '';
+
+  onOption(menuOption: string) {
+    this.menuOption = menuOption;
+  }
 
 }
