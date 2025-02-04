ce2629b3b0b8079f850527a047a049cb2e64f9e0
KevinEpn <kevin.asimbaya@epn.edu.ec>
2025-02-04 15:11:57 -0500

    Cambios en db.js para subir a la nube

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URL, { dbName: process.env.MONGO_DB_NAME });
-        console.log('MongoDB Connected...');
-    } catch (error) {
-        console.error(error.message);
-        process.exit(1);
-    }
+    // try {
+    //     await mongoose.connect(process.env.MONGO_URL, { dbName: process.env.MONGO_DB_NAME });
+    //     console.log('MongoDB Connected...');
+    // } catch (error) {
+    //     console.error(error.message);
+    //     process.exit(1);
+    // }
+    //Acá conectaremos la base de datos:
+    mongoose.connect(process.env.MONGO_URL, { dbName: process.env.MONGO_DB_NAME })
+    const db = mongoose.connection;
 };
 
 module.exports = connectDB;
