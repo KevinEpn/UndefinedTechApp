a4d9015d5bb75351a4431d0c23db62d4c850f596
KevinEpn <kevin.asimbaya@epn.edu.ec>
2025-02-04 23:14:54 -0500

    Fix CORS configuration to correct localhost URL

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 
 // Habilitar CORS
 app.use(cors({
-    origin: ['https://undefined-tech-app.netlify.app', 'http://locahost:4200/'], // Permitir solicitudes desde este dominio
+    origin: 'http://locahost:4200/', // Permitir solicitudes desde este dominio
     methods: ['GET','POST','PUT','DELETE'], // Métodos que se permiten
     allowedHeaders: ['Content-Type', 'Authorization'] // Cabeceras permitidas
 }));
